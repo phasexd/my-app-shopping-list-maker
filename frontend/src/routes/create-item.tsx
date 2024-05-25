a1ba81a915d676d81078485d6a83fddcddde7af3
Phasexd <adhil751012@gmail.com>
2024-05-25 14:42:41 +0530

    Added items and create items

--- frontend/src/routes/create-item.tsx
+++ frontend/src/routes/create-item.tsx
@@ -1,9 +1,160 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
-export const Route = createFileRoute('/create-item')({
+import { useForm } from "@tanstack/react-form";
+import {api} from "@/lib/api";
+
+
+export const Route = createFileRoute("/create-item")({
   component: CreateItem,
-})
+});
+
 
 function CreateItem() {
-  return <div className="p-2">Hello from CreateItem!</div>
+const navigate = useNavigate()
+  const form = useForm({
+    defaultValues: {
+      title: "",
+      amount: 0,
+      qty: 1,
+      unit: "Kg",
+    },
+    onSubmit: async ({ value }) => {
+      await new Promise((r) => setTimeout(r , 3000));
+
+      const res = await api.items.$post({json :value});
+     if(!res.ok){
+      throw new Error ("server error")
+     }
+     navigate({to : "/items"})
+    },
+  });
+
+  return (
+    <div className="p-2 max-w-xl m-auto">
+      <h2>Create New Item</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="title"
+          validators={{
+            onChange: ({ value }) =>
+              !value
+                ? "A Title is required"
+                : value.length < 3
+                  ? "Title must be at least 3 characters"
+                  : undefined,
+            onChangeAsyncDebounceMs: 500,
+            onChangeAsync: async ({ value }) => {
+              await new Promise((resolve) => setTimeout(resolve, 1000));
+              return value.includes("error") && 'No "error" allowed in Title';
+            },
+          }}
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Title:</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </>
+            );
+          }}
+        />
+
+        <form.Field
+          name="qty"
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Quantity:</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type="number"
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </>
+            );
+          }}
+        />
+        <form.Field
+          name="unit"
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Unit:</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </>
+            );
+          }}
+        />
+       <form.Field
+          name="amount"
+          children={(field) => {
+            // Avoid hasty abstractions. Render props are great!
+            return (
+              <>
+                <Label htmlFor={field.name}>Amount:</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type="number"
+                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </>
+            );
+          }}
+        />
+        <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <Button className="mt-5" type="submit" disabled={!canSubmit}>
+                {isSubmitting ? '...' : 'Submit'}
+              </Button>
+            )}
+          />
+      </form>
+    </div>
+  );
+}
+
+{
+  /* <form className="grid w-full max-w-sm items-center gap-1.5 mt-5"></form> */
 }
--- frontend/src/routes/items.tsx
+++ frontend/src/routes/items.tsx
@@ -1,9 +1,74 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {api } from "@/lib/api"
+import { useQuery } from '@tanstack/react-query';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 export const Route = createFileRoute('/items')({
   component: Items,
 })
 
+async function getAllItems() {
+  const res = await api.items.$get();
+  if(!res.ok){
+    throw new Error("server error")
+  }
+  const data = await res.json();
+  return data
+  
+}
+
 function Items() {
-  return <div className="p-2">Hello from Items!</div>
+   // Queries
+   const { isPending, error, data } = useQuery({ queryKey: ['get-all-items'], queryFn: getAllItems })
+
+
+   if (error) return 'An error has occurred: ' + error.message
+   return (
+   <div className='p-2 max-w-3xl m-auto'>
+   <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Title</TableHead>
+          <TableHead>Quantity</TableHead>
+          <TableHead>Units</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+      {isPending ? Array(3).fill(0).map((_, i) => (
+        <TableRow key={i}>
+        <TableCell className="font-medium"><Skeleton className="h-4" /></TableCell>
+        <TableCell><Skeleton className="h-4" /></TableCell>
+        <TableCell><Skeleton className="h-4" /></TableCell>
+        <TableCell className="text-right"><Skeleton className="h-4" /></TableCell>
+      </TableRow>
+      )) :
+        data?.items.map((items) => (
+          <TableRow key={items.id}>
+            <TableCell className="font-medium">{items.title}</TableCell>
+            <TableCell>{items.qty}</TableCell>
+            <TableCell>{items.unit}</TableCell>
+            <TableCell className="text-right">{items.amount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+
+    <pre>
+     
+    </pre>
+    </div>
+   );
 }
