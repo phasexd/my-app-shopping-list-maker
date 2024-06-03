8f4b021582a90d033956d2419429511057c474f1
Phasexd <adhil751012@gmail.com>
2024-06-03 11:27:07 +0530

    vercel test

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,18 @@ import {Hono} from 'hono';
 import {logger} from 'hono/logger'
 import { serveStatic } from 'hono/bun'
 import { itemsRoutes } from './Routes/items';
+import { cors } from 'hono/cors'
+import { handle } from 'hono/vercel'
+import type { PageConfig } from 'next'
+
+export const config: PageConfig = {
+    runtime: "edge",
+  }
+
 const app = new Hono()
 
 app.use('*', logger())
-
+app.use('/api/*', cors())
 
 const apiRoutes = app.basePath("/api").route("/items",itemsRoutes)
 
@@ -15,4 +23,6 @@ app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
 export type ApiRoutes = typeof apiRoutes
+export const GET = handle(app)
+export const POST = handle(app)
 
